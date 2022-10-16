f8a4966286b6ce89ce7b618ecf19cf5d9066f963
tonyatrushkova <antonina.trushkova@gmail.com>
2022-10-16 20:24:53 +0300

    Add Exercise_68 - function Akkerman

---
+++ Exercise_68/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n
+Console.Clear();
+Console.Write("Type a number M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Type a number N: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Sum: {AkkermanFun(m, n)}");
+
+int AkkermanFun(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return AkkermanFun(m - 1, 1);
+    }
+    else
+    {
+        return (AkkermanFun(m - 1, AkkermanFun(m, n - 1)));
+    }
+}
