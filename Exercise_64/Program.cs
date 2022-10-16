30a28528b7f2df227204a9c0d61503cbe375cf43
tonyatrushkova <antonina.trushkova@gmail.com>
2022-10-16 18:28:31 +0300

    Add Exercise_64 - descending order recursion

---
+++ Exercise_64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+Console.Clear();
+Console.Write("Type a number N: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+FillNumbers(n);
+
+void FillNumbers(int n)
+{
+    if (n > 0)
+    {
+        Console.Write("{0} ", n);
+        FillNumbers(n-1);
+    }
+}
