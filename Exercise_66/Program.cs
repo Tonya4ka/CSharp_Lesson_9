953509833329c8a73058c3c923ead25d3ade50a5
tonyatrushkova <antonina.trushkova@gmail.com>
2022-10-16 19:58:10 +0300

    Add Exercise_66 - Sum from M to M recursion

---
+++ Exercise_66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Рекурсивно
+Console.Clear();
+Console.Write("Type a number M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Type a number N: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Sum: {SumRecNumbers(m, n)}");
+SumRecNumbers(m, n);
+
+int SumRecNumbers(int m, int n)
+{
+    if (m < n)
+    {
+        return m + SumRecNumbers(m+1,n);
+    }
+    else
+    {
+        return n;
+    }
+}
